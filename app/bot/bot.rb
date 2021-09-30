12d3915ffeea105112d0a04bb7d685c98a8680a6
shmeit <shmeit@hotmail.com>
2021-09-30 02:27:23 -0400

    sevoo

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,5 +1,11 @@
 
+require 'facebook/messenger'
 include Facebook::Messenger
+
+Facebook::Messenger::Subscriptions.subscribe(
+  access_token: access_token,
+  subscribed_fields: %w[feed mention name]
+)
 
 Faceboook::Messenger::Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
--- config.ru
+++ config.ru
@@ -1,10 +1,8 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative "config/environment"
-require 'facebook/messenger'
-require_relative 'bot'
 
-run Facebook::Messenger::Server
+
 
 run Rails.application
 Rails.application.load_server
